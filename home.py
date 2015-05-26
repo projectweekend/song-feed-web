506be0bc2a424a202583487d6c402288bd0e5adf
Brian Hines <brian@projectweekend.net>
2015-05-26 05:46:55 -0500

    Work on home handler

--- home.py
+++ home.py
@@ -2,12 +2,14 @@ from datetime import date
 from datetime import timedelta
 from flask import render_template
 from server import app
-from utils.data import feed_data
+from utils.data import feed_items, feed_info
 
 
 @app.route('/')
 def home():
     end = date.today()
     start = end - timedelta(days=7)
-    feed_items = feed_data(start, end)
-    return render_template('home.html', feed_items=feed_items)
+    return render_template(
+        'home.html',
+        feed_info=feed_info(),
+        feed_items=feed_items(start, end))
--- utils/data.py
+++ utils/data.py
@@ -3,9 +3,10 @@ from dates import date_to_timestamp
 
 
 FEED_ITEMS = Table('feed_items')
+ACCOUNTS = Table('accounts')
 
 
-def feed_data(start, end):
+def feed_items(start, end):
     start = date_to_timestamp(start)
     end = date_to_timestamp(end)
     r = FEED_ITEMS.query_2(
@@ -14,3 +15,10 @@ def feed_data(start, end):
         date_posted__gte=start,
         date_posted__lte=end)
     return [dict(i.items()) for i in r]
+
+
+def feed_info():
+    r = ACCOUNTS.query_2(
+        attributes=('feed_title', ),
+        spotify_username__eq='musicman1381')
+    return [dict(i.items()) for i in r].pop()
